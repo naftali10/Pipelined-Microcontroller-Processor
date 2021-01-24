adfe13301428698ed187796299991cbd255c6054
Naftali Kizner <76939353+naftali10@users.noreply.github.com>
2021-01-24 08:11:21 +0200

    Use of package and macros

--- code/testbenches/RF_test.sv
+++ code/testbenches/RF_test.sv
@@ -1,25 +1,16 @@
-`define BUS_SIZE 8
-`define ENTRIES 4
-`define READ_PORTS 2
-`define WRITE_PORTS 1
-
-module stimulus;
-  RF #(.entries(`ENTRIES),
-       .data_bus_size(`BUS_SIZE),
-       .read_ports(`READ_PORTS),
-       .write_ports(`WRITE_PORTS)
-      ) RF_DUT (.clock(clock),
-                .src(src),
-                .dst(dst),
-                .datain(datain),
-                .wr_en(wr_en),
-                .dataout(dataout));
+module stimulus_for_RF;
+  RF RF_DUT (.clock(clock),
+             .src(src),
+             .dst(dst),
+             .datain(datain),
+             .wr_en(wr_en),
+             .dataout(dataout));
   logic clock;
-  logic [`BUS_SIZE-1:0] datain [`WRITE_PORTS-1:0];
-  logic [`BUS_SIZE-1:0] dataout [`READ_PORTS-1:0];
-  logic [$clog2(`ENTRIES)-1:0] src [`READ_PORTS-1:0];
-  logic [$clog2(`ENTRIES)-1:0] dst [`WRITE_PORTS-1:0];
-  logic wr_en [`WRITE_PORTS-1:0];
+  t_data datain [`WRITE_PORTS-1:0];
+  t_data dataout [`READ_PORTS-1:0];
+  t_RFadrs src [`READ_PORTS-1:0];	// read addresses
+  t_RFadrs dst [`WRITE_PORTS-1:0];	// write addresses
+  logic wr_en [`WRITE_PORTS-1:0];	// '1' is enableling writing
   
   assign src[0] = 1;
   assign src[1] = 3;
