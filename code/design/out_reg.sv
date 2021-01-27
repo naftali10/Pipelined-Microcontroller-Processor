467491184f40bc14c3765459793e29d1f8c708c0
Naftali Kizner <76939353+naftali10@users.noreply.github.com>
2021-01-27 17:45:52 +0200

    Create out_reg.sv

---
+++ code/design/out_reg.sv
@@ -0,0 +1,22 @@
+// Interface name: Outputs Register
+// Duty: Preparing signals for exititng the macine.
+//		 Shifting signal's clock.
+
+interface out_reg (input logic clock);
+  logic stalled, stalledx3;
+  logic dataoutvx2, dataoutvx3;
+  t_data dataoutx2, dataoutx3;
+  
+  always_ff @(posedge clock) begin
+    stalledx3 <= stalled;
+    dataoutvx3 <= dataoutvx2;
+    dataoutx3 <= dataoutx2;
+  end
+  
+  modport driver (output stalled,
+                  output dataoutvx2,
+                  output dataoutx2);
+  modport receiver (input stalledx3,
+                   input dataoutvx3,
+                   input dataoutx3);  
+endinterface
