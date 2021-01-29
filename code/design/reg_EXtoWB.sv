94700506684da7c2bd29b52af1068d5af282bdaa
Naftali Kizner <76939353+naftali10@users.noreply.github.com>
2021-01-29 15:32:28 +0200

    Update reg_EXtoWB.sv

--- code/design/reg_EXtoWB.sv
+++ code/design/reg_EXtoWB.sv
@@ -1,9 +1,10 @@
 // Interface name: Execution to Write Back Register
-// Duty: Transferring signals from EX stage to WB stage in teh pipeline.
+// Duty: Transferring signals from EX stage to WB stage in the pipeline.
 //		 Shifting signals' clock.
 
 interface reg_EXtoWB (input logic clock);
   logic wr_enx1, wr_enx2;
+  logic dataoutvx1, dataoutvx2;
   t_RFadrs dstx1, dstx2;
   t_data dataoutx1, dataoutx2;
   
@@ -11,12 +12,15 @@ interface reg_EXtoWB (input logic clock);
     wr_enx2 <= wr_enx1;
     dstx2 <= dstx1;
     dataoutx2 <= dataoutx1;
+    dataoutvx2 <= dataoutvx1;
   end
   
   modport driver (output wr_enx1,
                   output dstx1,
-                  output dataoutx1);
+                  output dataoutx1,
+                  output dataoutvx1);
   modport receiver (input wr_enx2,
                     input dstx2,
-                    input dataoutx2);
+                    input dataoutx2,
+                    input dataoutvx2);
 endinterface
