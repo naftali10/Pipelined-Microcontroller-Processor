c0493856a10a7bcd7e6b91b52483d79a41d88dfa
Naftali Kizner <76939353+naftali10@users.noreply.github.com>
2021-01-29 15:18:19 +0200

    Update Execute.sv

--- code/design/Execute.sv
+++ code/design/Execute.sv
@@ -4,17 +4,27 @@
 //		 Includes the ALU.
 
 module Execute (input logic internal_reset,
-                reg_IDtoEX,
-                reg_EXtoWB);
+                reg_IDtoEX.receiver reg_IDtoEX_,
+                reg_EXtoWB.driver reg_EXtoWB_);
   t_data A, B;
-  assign A = reg_IDtoEX.ALUsrc1x1===takeGPR?reg_IDtoEX.dat1x1:reg_IDtoEX.immx1;
-  assign B = reg_IDtoEX.ALUsrc2x1===takeGPR?reg_IDtoEX.dat2x1:reg_IDtoEX.immx1;
+  always_comb begin
+    if (reg_IDtoEX_.ALUsrc1x1===takeGPR)
+      assign A = reg_IDtoEX_.dat1x1;
+    if (reg_IDtoEX_.ALUsrc1x1===takeIMM)
+      assign A = reg_IDtoEX_.immx1;
+  end
+  always_comb begin
+    if (reg_IDtoEX_.ALUsrc2x1===takeGPR)
+      assign B = reg_IDtoEX_.dat2x1;
+    if (reg_IDtoEX_.ALUsrc2x1===takeIMM)
+      assign B = reg_IDtoEX_.immx1;
+  end
   ALU ALU_inst (.A(A),
                 .B(B),
-                .op(reg_IDtoEX.ALUopx1),
-                .result(reg_EXtoWB.dataoutx1));
+                .op(reg_IDtoEX_.ALUopx1),
+                .result(reg_EXtoWB_.dataoutx1));
   
-  assign reg_EXtoWB.wr_enx1 = internal_reset?0:reg_IDtoEX.wr_enx1;
-  assign reg_EXtoWB.dataoutvx1 = internal_reset?0:reg_IDtoEX.dataoutvx1;
-  assign reg_EXtoWB.dstx1 = reg_IDtoEX.dstx1;
+  assign reg_EXtoWB_.wr_enx1 = internal_reset?0:reg_IDtoEX_.wr_enx1;
+  assign reg_EXtoWB_.dataoutvx1 = internal_reset?0:reg_IDtoEX_.dataoutvx1;
+  assign reg_EXtoWB_.dstx1 = reg_IDtoEX_.dstx1;
 endmodule
