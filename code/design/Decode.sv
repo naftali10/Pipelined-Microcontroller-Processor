672848617d79a50ae62ba050ec8a382172d07396
Naftali Kizner <76939353+naftali10@users.noreply.github.com>
2021-01-29 15:17:29 +0200

    Update Decode.sv

--- code/design/Decode.sv
+++ code/design/Decode.sv
@@ -3,9 +3,9 @@
 //		 Receives inputs to the machine and interprets them.
 //		 Includes the controller and the register file.
 
-module Decode (ifc_inputs ifc_inputs_,
-               ifc_WB ifc_WB_,
-               reg_IDtoEX reg_IDtoEX_,
+module Decode (ifc_inputs.receiver ifc_inputs_,
+               ifc_WB.receiver ifc_WB_,
+               reg_IDtoEX.driver reg_IDtoEX_,
                output logic internal_reset,
                output logic stalled);
   controller controller_inst (.clock(ifc_inputs_.clock),
@@ -23,11 +23,11 @@ module Decode (ifc_inputs ifc_inputs_,
                               .stalled(stalled));
                                                            
   RF RF_inst (.clock(ifc_inputs_.clock),
-              .src({t_RFadrs'(ifc_inputs_.src2),t_RFadrs'(ifc_inputs_.src1)}),
+              .src('{t_RFadrs'(ifc_inputs_.src2),t_RFadrs'(ifc_inputs_.src1)}),
               .dst(ifc_WB_.dstx2),
               .datain(ifc_WB_.dataoutx2),
               .wr_en(ifc_WB_.wr_enx2),
-              .dataout({reg_IDtoEX_.dat2x0,reg_IDtoEX_.dat1x0}));
+              .dataout('{reg_IDtoEX_.dat2x0,reg_IDtoEX_.dat1x0}));
   
   assign reg_IDtoEX_.immx0 = ifc_inputs_.imm;
   assign reg_IDtoEX_.dstx0 = t_RFadrs'(ifc_inputs_.dst);
