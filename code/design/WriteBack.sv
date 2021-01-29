f70618f482dc3d31d1688ebc5f13fd5d26303fe5
Naftali Kizner <76939353+naftali10@users.noreply.github.com>
2021-01-29 15:25:29 +0200

    Update WriteBack.sv

--- code/design/WriteBack.sv
+++ code/design/WriteBack.sv
@@ -6,14 +6,14 @@
 
 module WriteBack (input logic internal_reset,
                   input logic stalled,
-                  reg_EXtoWB,
-                  reg_out,
-                  ifc_WB);
-  assign reg_out.stalled = stalled;
-  assign reg_out.dataoutvx2 = internal_reset?0:reg_EXtoWB.dataoutvx2;
-  assign reg_out.dataoutx2 = reg_EXtoWB.dataoutx2;
+                  reg_EXtoWB.receiver reg_EXtoWB_,
+                  reg_out.driver reg_out_,
+                  ifc_WB.driver ifc_WB_);
+  assign reg_out_.stalled = stalled;
+  assign reg_out_.dataoutvx2 = internal_reset?0:reg_EXtoWB_.dataoutvx2;
+  assign reg_out_.dataoutx2 = reg_EXtoWB_.dataoutx2;
   
-  assign ifc_WB.wr_enx2 = internal_reset?0:reg_EXtoWB.wr_enx2;
-  assign ifc_WB.dstx2 = reg_EXtoWB.dstx2;
-  assign ifc_WB.dataoutx2 = reg_EXtoWB.dataoutx2;
+  assign ifc_WB_.wr_enx2 = internal_reset?0:reg_EXtoWB_.wr_enx2;
+  assign ifc_WB_.dstx2 = reg_EXtoWB_.dstx2;
+  assign ifc_WB_.dataoutx2 = reg_EXtoWB_.dataoutx2;
 endmodule
